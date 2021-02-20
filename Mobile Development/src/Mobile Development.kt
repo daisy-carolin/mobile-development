2d1d6b0abdb668ddfce51660f09aab2b10317e3d
student <daisy-carolin@gmail.com>
2021-02-21 00:18:18 +0300

    my first assignment

---
+++ Mobile Development/src/Mobile Development.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullname="christine kageha"
+    println(fullname)
+    var age= 22
+    println(age)
+    var phonenumber="0757702813"
+    println(phonenumber)
+    var weight= 65
+    println(weight)
+    var citizen="kenyan"
+    println(citizen)
+}
